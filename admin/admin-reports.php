d3539298414fc229abf245151c81b548e978d319
rensequitin <rensequitin@gmail.com>
2018-05-31 19:54:28 +0800

    Add app icon

--- admin/admin-reports.php
+++ admin/admin-reports.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Admin</title>
+  <link rel="icon" href="images/icon.ico">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Admin</title>
   <!-- Tell the browser to be responsive to screen width -->
+  <link rel="icon" href="images/icon.ico">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
